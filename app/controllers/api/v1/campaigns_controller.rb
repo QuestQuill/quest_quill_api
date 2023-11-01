66109391134f9fe0df857283698f837b563f0733
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-11-01 15:30:11 -0500

    Create the show test for the campaign controller spec and its corresponding route and controller method

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::CampaignsController < ApplicationController
   def index
     render json: Campaign.all
   end
+
+  def show
+    render json: Campaign.find(params[:id])
+  end
 end
--- spec/requests/api/v1/campaigns_request_spec.rb
+++ spec/requests/api/v1/campaigns_request_spec.rb
@@ -1,11 +1,27 @@
 require 'rails_helper'
 
 describe "Campaigns API" do
-  it "sends a list of campaigns" do
-    create_list(:campaign, 3)
+  before (:each) do
+    load_test_data
+  end
 
+  it "sends a list of campaigns" do
     get '/api/v1/campaigns'
 
     expect(response).to be_successful
+
+    campaigns = JSON.parse(response.body)
+    
+    # add more robust testing!
+  end
+
+  it "can get a single Campaign" do
+    get "/api/v1/campaigns/#{@campaign1.id}"
+
+    campaign = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    # add more robust testing!
   end
 end
