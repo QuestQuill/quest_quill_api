caed41f68eccf33e6441c8f9212a768e7dcd71fc
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-11-01 23:51:39 -0500

    Add the bcrypt gem to the project for password encryption, and modify the schema to reflect the change from password to password_digest:

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_02_024433) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
