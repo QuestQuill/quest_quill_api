8c1d91c12f2e63db49763648bdabe49a5b142bac
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-11-01 23:53:00 -0500

    Create the 'create' route for users, and modify the validations for the users model

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :campaigns, only: [:index, :show]
-      resources :users, only: [:index, :show]
+      resources :users, only: [:index, :show, :create]
     end
   end
 end
