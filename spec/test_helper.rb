6161cab0ae8e776a3e9569eb38cbf5b796dabafc
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-11-02 23:51:41 -0500

    Create the necessary test files for the town model and add town objects to the test_helper file

--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -2,4 +2,5 @@ def load_test_data
   @users = create_list(:user, 2)
   @campaigns = create_list(:campaign, 3, user: @users.first)
   @campaign1 = create(:campaign)
+  @towns = create_list(:town, 2, campaign: @campaign1)
 end
