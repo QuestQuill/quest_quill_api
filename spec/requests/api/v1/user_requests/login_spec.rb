f1707d6d602a43ea05351dcd16dab8bac2b2401f
Captainlearyo <clayoleary@gmail.com>
2023-11-05 17:00:52 -0600

    complete user login

---
+++ spec/requests/api/v1/user_requests/login_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'User Login', type: :request do
+  before(:each) do
+    load_test_data
+  end
+  it 'Happy Path Login by email and password' do
+
+    user_params = {
+      email: 'user1@example.com',
+      password: 'password1'
+    }
+
+    post '/api/v1/users/login', params: user_params
+
+    expect(response).to have_http_status(:ok)
+
+  end
+
+  it 'Sad Path Login by email and password wrong info' do
+    user_params = {
+      email: 'user1@example.com',
+      password: 'wrong_password'
+    }
+
+    post '/api/v1/users/login', params: user_params
+
+    expect(response).to have_http_status(:unauthorized)
+  end
+end
