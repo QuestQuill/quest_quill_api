07bd2163a0dad101cdd70562fb7da485cca57d3d
Captainlearyo <clayoleary@gmail.com>
2023-11-06 11:18:22 -0600

    add method tests

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Campaign, type: :model do
+  describe "relationships" do
+    it { should belong_to :user }
+    it { should have_many :towns }
+  end
+
+  describe "validations" do
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:player_num)}
+    it {should validate_presence_of(:themes)}
+  end
+
   pending "add some examples to (or delete) #{__FILE__}"
+  
 end
