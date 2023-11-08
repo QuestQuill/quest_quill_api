ca432ed9b33ec59e3757dbb7624dd035d2d8688a
Eliza Keating <ek.elizakeating@gmail.com>
2023-11-07 18:06:13 -0700

    feat: update search_by_username to by email and add search_by_token class methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,11 @@ class User < ApplicationRecord
   
   has_many :campaigns
 
-  def self.search_by_username(username)
-    where('lower(username) LIKE ?', "%#{username.downcase}%")
+  def self.search_by_email(email)
+    where('lower(email) LIKE ?', "%#{email.downcase}%")
+  end
+
+  def self.search_by_token(token)
+    where('token = ? AND token_expiration > ?', token, Time.now)
   end
 end
