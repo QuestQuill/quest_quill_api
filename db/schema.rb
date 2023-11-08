8792fc8c853ef26c0bef70aafbf126f68a3881fe
Eliza Keating <ek.elizakeating@gmail.com>
2023-11-07 18:00:10 -0700

    feat: add token and token_expiration columns to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_05_205029) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_07_195219) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_05_205029) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
+    t.datetime "token_expiration"
   end
 
   add_foreign_key "campaigns", "users"
