60122728f8439de7f27a50604efa1046644096da
Eliza Keating <ek.elizakeating@gmail.com>
2023-11-07 18:03:02 -0700

    test: update all tests for user search spec to match new functionality

--- spec/requests/api/v1/user_requests/search_spec.rb
+++ spec/requests/api/v1/user_requests/search_spec.rb
@@ -4,16 +4,15 @@ RSpec.describe 'User Search', type: :request do
   before(:each) do
     load_test_data
   end
-  it 'Happy Path Searches for User By UserName' do
+  it 'Happy Path Searches for User By email' do
+    email_query = 'user1@example.com'
 
-    search_query = 'user1'
-
-    get "/api/v1/users/search?query=#{search_query}"
+    get "/api/v1/users/search?email=#{email_query}"
 
     expect(response).to be_successful
-
+    
     json_response = JSON.parse(response.body)
-
+    
     expect(json_response).to be_an(Array)
 
     expect(json_response.length).to be >= 1
@@ -22,14 +21,38 @@ RSpec.describe 'User Search', type: :request do
     expect(json_response[0]['email']).to eq('user1@example.com')
   end
 
-  it 'Sad Path User name not found' do
+  it 'happy path searches for user by token' do
+    token_query = 'abcdefg'
 
-    search_query = 'Bobby Brown'
+    get "/api/v1/users/search?token=#{token_query}"
+
+    expect(response).to be_successful
+
+    json_response = JSON.parse(response.body)
     
-    get "/api/v1/users/search?query=#{search_query}"
+    expect(json_response).to be_an(Array)
+
+    expect(json_response.length).to be >= 1
+
+    expect(json_response[0]['username']).to eq('user1')
+    expect(json_response[0]['email']).to eq('user1@example.com')
+  end
+
+  it 'Sad Path email not found' do
+
+    email_query = 'Bobby Brown'
+    
+    get "/api/v1/users/search?email=#{email_query}"
 
     expect(response).to have_http_status(:bad_request)
+  end
 
+  it 'Sad Path token not found' do
 
+    token_query = 'Bobby Brown'
+    
+    get "/api/v1/users/search?token=#{token_query}"
+
+    expect(response).to have_http_status(:bad_request)
   end
 end
