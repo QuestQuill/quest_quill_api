c4c815333ebceb91a3d9d16375bf6664b5e0a097
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-11-27 20:45:18 -0600

    Update the campaign controller's create method and update the display in the campaign serializer to account for the theme models

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -11,7 +11,14 @@ class Api::V1::CampaignsController < ApplicationController
 
 
   def create
-    render json: CampaignSerializer.new(Campaign.create(campaign_params))
+    themes = find_themes(campaign_params) 
+    campaign = Campaign.new(campaign_params)
+
+    if campaign.save
+      render json: CampaignSerializer.new(campaign), status: 201
+    else
+      render json: { error: "An error has occured while creating your campaign" }, status: 401
+    end
   end
   
   def update
@@ -27,11 +34,21 @@ class Api::V1::CampaignsController < ApplicationController
     @campaign = Campaign.find(params[:id])
     @campaign.campaign_photo.attach(params[:campaign_photo])
     render json: CampaignSerializer.new(@campaign)
-  end
+  end 
 
   private 
 
   def campaign_params
-    params.permit(:name, :player_num, :user_id, :campaign_photo, themes: [])
+    params.permit(:name, :player_num, :user_id, :campaign_photo, theme_ids: [])
   end
+
+  def find_themes(parameters)
+    theme_ids = parameters[:theme_ids]
+    themes_by_id = theme_ids.map do |theme_id|
+      Theme.find(theme_id)
+    end
+    themes_by_id.map do |theme|
+      theme.name
+    end
+  end 
 end
--- app/serializers/campaign_serializer.rb
+++ app/serializers/campaign_serializer.rb
@@ -3,7 +3,11 @@ class CampaignSerializer
 
   set_type :campaign
   set_id :id
-  attributes :name, :player_num, :themes, :user_id, :campaign_photo
+  attributes :name, :player_num, :user_id, :themes, :campaign_photo
+
+  attribute :themes do |campaign|
+    campaign.themes.map(&:name)
+  end
 end
 
 # Potential to add the towns, npcs, quests relationships to the serializer
